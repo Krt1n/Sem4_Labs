2e0a50643c1a0825a21b73ff94a6d0eeeae646e0
Krtin Vishnu Vardhan <krtinstudiez@gmail.com>
2026-04-09 15:48:55 +0530

    Add files via upload

---
+++ OSL/Lab11/q1.c
@@ -0,0 +1,172 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+
+void fcfs(int requests[], int n, int head){
+    int total= 0, current=head;
+    printf("\n---FCFS--\n");
+    printf("Order of service: %d", head);
+    for(int i=0;i<n;i++){
+        printf(" -> %d", requests[i]);
+        total+=abs(requests[i]-current);
+        current=requests[i];
+    }
+    printf("\nTotal Cylinder Movements: %d\n",total);
+}
+
+void sstf(int requests[], int n, int head){
+    int total=0, current=head;
+    int visited[100]={0};
+    int req[100];
+
+    for(int i=0;i<n;i++){
+        req[i] = requests[i];
+    }
+
+    printf("\n--- SSTF ---\n");
+    printf("Order of service: %d",head);
+
+    for(int count=0;count<n;count++){
+        int minDist=99999, minIdx=-1;
+
+        for(int i=0;i<n;i++){
+            if(!visited[i]){
+                int dist = abs(req[i]-current);
+                if(dist < minDist){
+                    minDist = dist;
+                    minIdx = i;
+                }
+            }
+        }
+
+        if(minIdx != -1){
+            visited[minIdx]=1;
+            total+=minDist;
+            current = req[minIdx];
+            printf(" -> %d", req[minIdx]);
+        }
+    }
+
+    printf("\nTotal Cylinder Movements: %d\n", total);
+}
+
+/* SCAN */
+void scan(int requests[], int n, int head, int disk_size){
+    int total = 0, current = head;
+    int req[100];
+
+    for(int i=0;i<n;i++)
+        req[i]=requests[i];
+
+    for(int i=0;i<n-1;i++){
+        for(int j=0;j<n-i-1;j++){
+            if(req[j] > req[j+1]){
+                int t = req[j];
+                req[j] = req[j+1];
+                req[j+1] = t;
+            }
+        }
+    }
+
+    int pos = 0;
+    while(pos < n && req[pos] < head)
+        pos++;
+
+    printf("\n--- SCAN ---\n");
+    printf("\nOrder of Service: %d", head);
+
+    for(int i=pos;i<n;i++){
+        printf(" -> %d", req[i]);
+        total += abs(req[i] - current);
+        current = req[i];
+    }
+
+    printf(" -> %d", disk_size - 1);
+    total += abs((disk_size - 1) - current);
+    current = disk_size - 1;
+
+    for(int i=pos-1;i>=0;i--){
+        printf(" -> %d", req[i]);
+        total += abs(req[i] - current);
+        current = req[i];
+    }
+
+    printf("\nTotal Cylinder Movements: %d\n", total);
+}
+
+/* C-SCAN */
+void cscan(int requests[], int n, int head, int disk_size){
+    int total = 0, current = head;
+    int req[100];
+
+    for(int i=0;i<n;i++)
+        req[i]=requests[i];
+
+    for(int i=0;i<n-1;i++){
+        for(int j=0;j<n-i-1;j++){
+            if(req[j] > req[j+1]){
+                int t = req[j];
+                req[j] = req[j+1];
+                req[j+1] = t;
+            }
+        }
+    }
+
+    int pos = 0;
+    while(pos < n && req[pos] < head)
+        pos++;
+
+    printf("\n--- C-SCAN ---\n");
+    printf("\nOrder of Service: %d", head);
+
+    for(int i=pos;i<n;i++){
+        printf(" -> %d", req[i]);
+        total += abs(req[i] - current);
+        current = req[i];
+    }
+
+    if(current != disk_size - 1){
+        printf(" -> %d", disk_size - 1);
+        total += abs((disk_size - 1) - current);
+        current = disk_size - 1;
+    }
+
+    printf(" -> 0");
+    total += (disk_size - 1);
+    current = 0;
+
+    for(int i=0;i<pos;i++){
+        printf(" -> %d", req[i]);
+        total += abs(req[i] - current);
+        current = req[i];
+    }
+
+    printf("\nTotal Cylinder Movements: %d\n", total);
+}
+
+int main(){
+    int n, head, disk_size;
+
+    printf("Enter total number of cylinders: ");
+    scanf("%d",&disk_size);
+
+    printf("Enter initial head position     :");
+    scanf("%d",&head);
+
+    printf("Enter the number of requests    :");
+    scanf("%d",&n);
+
+    int requests[100];
+
+    printf("Enter %d disk requests      :",n);
+    for(int i = 0; i < n; i++){
+        scanf("%d",&requests[i]);
+    }
+
+    fcfs(requests,n,head);
+    sstf(requests,n,head);
+    scan(requests,n,head,disk_size);
+    cscan(requests,n,head,disk_size);
+
+    return 0;
+}
